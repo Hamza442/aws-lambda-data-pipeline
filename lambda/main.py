1504934aaf2825861b5815a9a556dde4d3a5a5d1
Khalid Ahmed <khalid.ahmed@nexdegree.com>
2024-03-21 14:37:41 +0500

    optime reading file

--- lambda/main.py
+++ lambda/main.py
@@ -3,8 +3,8 @@ import json
 import datetime
 from datetime import datetime as dt
 import logging
-from helpers import extract_file_name, read_from_s3, write_to_s3, save_job_run_details, rename_columns,\
-    get_mapping_tables,parse_s3_response
+from helpers import extract_file_name, read_file_contents_from_s3, write_to_s3, save_job_run_details, rename_columns,\
+    get_mapping_tables
 from clean import trim_and_upper, cleaning_fuel_type, clean_transmission, clean_engine_size, clean_cylinders,\
     cleaning_hp, clean_by_type, clean_seller_type, clean_for_duration, clean_body_type, clean_data, cleaning_spec\
     , cleaningModel
@@ -63,20 +63,19 @@ def process_file(bucket, key):
         job_start_time = int(time.mktime(dt.now().timetuple()))
         start_time = datetime.datetime.now()
         # Reading file
-        res = read_from_s3(bucket, key)
-        car_data = parse_s3_response(res)
-        cols_renamed = rename_columns(car_data)
+        contents = read_file_contents_from_s3(bucket, key)
+        cols_renamed = rename_columns(contents)
         mapping_tables = get_mapping_tables(secret, region)
         cleaned_data = clean_data(cols_renamed, cleaning_functions, mapping_tables)
         file_name = extract_file_name(bucket)
-        res = write_to_s3(cleaned_data, destination_bucket, destination_prefix, file_name)
+        response = write_to_s3(cleaned_data, destination_bucket, destination_prefix, file_name)
         # time for dynamodb logging
         end_time = datetime.datetime.now()
         elapsed_time = end_time - start_time
         elapsed_seconds = elapsed_time.total_seconds()
         save_job_run_details(
-            lambda_job_id, job_start_time, f"s3://{bucket}/{key}", res['destination_file_name'], start_time, end_time,
-            elapsed_seconds, res['status_code'], dynamodb_table)
+            lambda_job_id, job_start_time, f"s3://{bucket}/{key}", response['destination_file_name'], start_time, end_time,
+            elapsed_seconds, response['status_code'], dynamodb_table)
         logger.info(f"Processing completed for file:e s3://{bucket}/{key}")
     except Exception:
         logger.error(f"Error while processing the file s3://{bucket}/{key}")
